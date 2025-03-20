6f7f4551b195de7abd25012279dd83ce9f7b1270
Burtarn <robin.andersson0125@gmail.com>
2025-03-20 21:31:33 +0100

    update

--- nuggetdashboard/src/components/footer/footer.jsx
+++ nuggetdashboard/src/components/footer/footer.jsx
@@ -11,28 +11,28 @@ import {
 export default function App() {
   return (
     <MDBFooter bgColor="light" className="text-center text-lg-start text-muted">
-      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-        <div className="me-5 d-none d-lg-block">
-          <span>Get connected with us on social networks:</span>
+      <section className="d-flex border-bottom justify-content-center justify-content-lg-between p-4">
+        <div className="d-lg-block d-none me-5">
+          <span>Följ oss på sociala medier:</span>
         </div>
 
         <div>
-          <a href="" className="me-4 text-reset">
+          <a href="" className="text-reset me-4">
             <MDBIcon color="secondary" fab icon="facebook-f" />
           </a>
-          <a href="" className="me-4 text-reset">
+          <a href="" className="text-reset me-4">
             <MDBIcon color="secondary" fab icon="twitter" />
           </a>
-          <a href="" className="me-4 text-reset">
+          <a href="" className="text-reset me-4">
             <MDBIcon color="secondary" fab icon="google" />
           </a>
-          <a href="" className="me-4 text-reset">
+          <a href="" className="text-reset me-4">
             <MDBIcon color="secondary" fab icon="instagram" />
           </a>
-          <a href="" className="me-4 text-reset">
+          <a href="" className="text-reset me-4">
             <MDBIcon color="secondary" fab icon="linkedin" />
           </a>
-          <a href="" className="me-4 text-reset">
+          <a href="" className="text-reset me-4">
             <MDBIcon color="secondary" fab icon="github" />
           </a>
         </div>
@@ -41,44 +41,44 @@ export default function App() {
       <section className="">
         <MDBContainer className="text-center text-md-start mt-5">
           <MDBRow className="mt-3">
-            <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
+            <MDBCol md="3" lg="4" xl="3" className="mb-4 mx-auto">
               <h6 className="text-uppercase fw-bold mb-4">
                 <MDBIcon color="secondary" icon="gem" className="me-3" />
                 Bitcoin Bros
               </h6>
               <p>
-                Are you ready to revolutionize the way you think about money?
-                Say goodbye to traditional banking and hello to a limitless,
-                decentralized financial world with Bitcoin Bros — the
-                cutting-edge Bitcoin company that's shaking up the game!
+              Är du redo att revolutionera ditt sätt att tänka på pengar?
+              Säg adjö till traditionell bankverksamhet och hej till en gränslös,
+              decentraliserad finansiell värld med Bitcoin Bros – det
+              banbrytande Bitcoin-företaget som förändrar spelplanen!
               </p>
             </MDBCol>
 
-            <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+            <MDBCol md="2" lg="2" xl="2" className="mb-4 mx-auto">
+              <h6 className="text-uppercase fw-bold mb-4">Produkter</h6>
               <p>
                 <a href="#!" className="text-reset">
-                  Money
+                  Pengar
                 </a>
               </p>
               <p>
                 <a href="#!" className="text-reset">
-                  Money
+                Pengar
                 </a>
               </p>
               <p>
                 <a href="#!" className="text-reset">
-                  Money
+                Pengar
                 </a>
               </p>
               <p>
                 <a href="#!" className="text-reset">
-                  Money
+                Pengar
                 </a>
               </p>
             </MDBCol>
 
-            <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
+            <MDBCol md="3" lg="2" xl="2" className="mb-4 mx-auto">
               <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
               <p>
                 <NavLink to="/" className="text-reset">
@@ -87,26 +87,26 @@ export default function App() {
               </p>
               <p>
                 <NavLink to="/calc" className="text-reset">
-                  Calculate savings
+                  Support
                 </NavLink>
               </p>
               <p>
                 <NavLink to="/about" className="text-reset">
-                  About us
+                  Sparkalkylator
                 </NavLink>
               </p>
               <p>
                 <NavLink to="/about/contact" className="text-reset">
-                  Contact
+                  Dashboard
                 </NavLink>
               </p>
             </MDBCol>
 
-            <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
+            <MDBCol md="4" lg="3" xl="3" className="mb-4 mb-md-0 mx-auto">
               <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
               <p>
                 <MDBIcon color="secondary" icon="home" className="me-2" />
-                On the Moon, Space 0000, Space
+                På Månen, 000 00, Rymden
               </p>
               <p>
                 <MDBIcon color="secondary" icon="envelope" className="me-3" />
@@ -126,7 +126,7 @@ export default function App() {
       </section>
 
       <div
-        className="text-center p-4"
+        className="p-4 text-center"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
       >
         © 2025 Copyright:
